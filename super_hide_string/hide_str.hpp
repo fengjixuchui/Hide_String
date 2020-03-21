2df636e6bf98554a79bd547096e7b9580bc2aaea
lurumdare <8020186+lurumdare@users.noreply.github.com>
2020-03-21 20:54:48 +0300

    refactor

--- super_hide_string/hide_str.hpp
+++ super_hide_string/hide_str.hpp
@@ -10,17 +10,16 @@
 #define HIDE_STR(hide, s) auto (hide) = HideString<sizeof(s) - 1, __COUNTER__ >(s, std::make_index_sequence<sizeof(s) - 1>())
 #define PRINT_HIDE_STR(s) (HideString<sizeof(s) - 1, __COUNTER__ >(s, std::make_index_sequence<sizeof(s) - 1>()).decrypt())
 
-inline uint32_t Murmur3(const void* key, int len, unsigned int seed)
+inline uint32_t murmur3(const void* key, int len, unsigned int seed)
 {
 	const unsigned int m = 0x5bd1e995;
 	const int r = 24;
 	unsigned int l = len;
-	const unsigned char* data = (const unsigned char*)key;
+	const auto* data = static_cast<const unsigned char*>(key);
 	unsigned int h = seed;
-	unsigned int k;
 	while (len >= 4)
 	{
-		k = *(unsigned int*)data;
+		unsigned int k = *(unsigned int*)data;
 		MMIX(h, k);
 		data += 4;
 		len -= 4;
@@ -31,6 +30,7 @@ inline uint32_t Murmur3(const void* key, int len, unsigned int seed)
 	case 3: t ^= data[2] << 16;
 	case 2: t ^= data[1] << 8;
 	case 1: t ^= data[0];
+	default: ;
 	};
 	MMIX(h, t);
 	MMIX(h, l);
@@ -48,12 +48,12 @@ constexpr auto seed =
 	+ static_cast<int>(time[1]) * 3600 + static_cast<int>(time[0]) * 36000;
 
 template <int N>
-struct RandomGeneratorString
+struct random_generator_string
 {
 private:
 	static constexpr unsigned a = 16807;
 	static constexpr unsigned m = 2147483647;
-	static constexpr unsigned s = RandomGeneratorString<N - 1>::value;
+	static constexpr unsigned s = random_generator_string<N - 1>::value;
 	static constexpr unsigned lo = a * (s & 0xFFFF);
 	static constexpr unsigned hi = a * (s >> 16);
 	static constexpr unsigned lo2 = lo + ((hi & 0x7FFF) << 16);
@@ -66,24 +66,24 @@ public:
 };
 
 template <>
-struct RandomGeneratorString<0>
+struct random_generator_string<0>
 {
 	static constexpr unsigned value = seed;
 };
 
 template <int N, int M>
-struct RandomInt
+struct random_int
 {
-	static constexpr auto value = RandomGeneratorString<N + 1>::value % M;
+	static constexpr auto value = random_generator_string<N + 1>::value % M;
 };
 
 template <int N>
-struct RandomChar
+struct random_char
 {
-	static const char value = static_cast<char>(1 + RandomInt<N, 0x7F - 1>::value);
+	static const char value = static_cast<char>(1 + random_int<N, 0x7F - 1>::value);
 };
 
-class XTEA3
+class xtea3
 {
 public:
 	uint8_t* data_ptr = nullptr;
@@ -91,32 +91,30 @@ public:
 	uint32_t size_decrypt_data = 0;
 
 protected:
-	uint32_t rol(uint32_t base, uint32_t shift)
+	static uint32_t rol(uint32_t base, uint32_t shift)
 	{
-		uint32_t res;
 		/* only 5 bits of shift are significant*/
 		shift &= 0x1F;
-		res = (base << shift) | (base >> (32 - shift));
+		const auto res = base << shift | base >> 32 - shift;
 		return res;
 	};
 
-	void xtea3_encipher(unsigned int num_rounds, uint32_t* v, const uint32_t* k)
+	static void xtea3_encipher(unsigned int num_rounds, uint32_t* v, const uint32_t* k)
 	{
-		unsigned int i;
-		uint32_t a, b, c, d, sum = 0, t, delta = 0x9E3779B9;
-		sum = 0;
-		a = v[0] + k[0];
-		b = v[1] + k[1];
-		c = v[2] + k[2];
-		d = v[3] + k[3];
-		for (i = 0; i < num_rounds; i++)
+		const auto delta = 0x9E3779B9;
+		unsigned sum = 0;
+		auto a = v[0] + k[0];
+		auto b = v[1] + k[1];
+		auto c = v[2] + k[2];
+		auto d = v[3] + k[3];
+		for (unsigned int i = 0; i < num_rounds; i++)
 		{
-			a += (((b << 4) + rol(k[(sum % 4) + 4], b)) ^
-				(d + sum) ^ ((b >> 5) + rol(k[sum % 4], b >> 27)));
+			a += (b << 4) + rol(k[sum % 4 + 4], b) ^
+				d + sum ^ (b >> 5) + rol(k[sum % 4], b >> 27);
 			sum += delta;
-			c += (((d << 4) + rol(k[((sum >> 11) % 4) + 4], d)) ^
-				(b + sum) ^ ((d >> 5) + rol(k[(sum >> 11) % 4], d >> 27)));
-			t = a;
+			c += (d << 4) + rol(k[(sum >> 11) % 4 + 4], d) ^
+				b + sum ^ (d >> 5) + rol(k[(sum >> 11) % 4], d >> 27);
+			const auto t = a;
 			a = b;
 			b = c;
 			c = d;
@@ -128,26 +126,26 @@ protected:
 		v[3] = d ^ k[7];
 	};
 
-	void xtea3_decipher(unsigned int num_rounds, uint32_t* v, const uint32_t* k)
+	static void xtea3_decipher(const unsigned int num_rounds, uint32_t* v, const uint32_t* k)
 	{
-		unsigned int i;
-		uint32_t a, b, c, d, t, delta = 0x9E3779B9, sum = delta * num_rounds;
-		d = v[3] ^ k[7];
-		c = v[2] ^ k[6];
-		b = v[1] ^ k[5];
-		a = v[0] ^ k[4];
-		for (i = 0; i < num_rounds; i++)
+		const auto delta = 0x9E3779B9;
+		auto sum = delta * num_rounds;
+		auto d = v[3] ^ k[7];
+		auto c = v[2] ^ k[6];
+		auto b = v[1] ^ k[5];
+		auto a = v[0] ^ k[4];
+		for (unsigned int i = 0; i < num_rounds; i++)
 		{
-			t = d;
+			const auto t = d;
 			d = c;
 			c = b;
 			b = a;
 			a = t;
-			c -= (((d << 4) + rol(k[((sum >> 11) % 4) + 4], d)) ^
-				(b + sum) ^ ((d >> 5) + rol(k[(sum >> 11) % 4], d >> 27)));
+			c -= (d << 4) + rol(k[(sum >> 11) % 4 + 4], d) ^
+				b + sum ^ (d >> 5) + rol(k[(sum >> 11) % 4], d >> 27);
 			sum -= delta;
-			a -= (((b << 4) + rol(k[(sum % 4) + 4], b)) ^
-				(d + sum) ^ ((b >> 5) + rol(k[sum % 4], b >> 27)));
+			a -= (b << 4) + rol(k[sum % 4 + 4], b) ^
+				d + sum ^ (b >> 5) + rol(k[sum % 4], b >> 27);
 		}
 		v[3] = d - k[3];
 		v[2] = c - k[2];
@@ -155,60 +153,52 @@ protected:
 		v[0] = a - k[0];
 	};
 
-	void xtea3_data_crypt(uint8_t* inout, uint32_t len, bool encrypt, const uint32_t* key)
+	static void xtea3_data_crypt(uint8_t* inout, const uint32_t len, const bool encrypt, const uint32_t* key)
 	{
-		static unsigned char dataArray[BLOCK_SIZE];
-		for (int i = 0; i < len / BLOCK_SIZE; i++)
+		static unsigned char data_array[BLOCK_SIZE];
+		for (unsigned int i = 0; i < len / BLOCK_SIZE; i++)
 		{
-			memcpy(dataArray, inout, BLOCK_SIZE);
+			memcpy(data_array, inout, BLOCK_SIZE);
 			if (encrypt)
-				xtea3_encipher(48, (uint32_t*)dataArray, key);
+				xtea3_encipher(48, reinterpret_cast<uint32_t*>(data_array), key); // TODO: reinterpret_cast check
 			else
-				xtea3_decipher(48, (uint32_t*)dataArray, key);
-			memcpy(inout, dataArray, BLOCK_SIZE);
+				xtea3_decipher(48, reinterpret_cast<uint32_t*>(data_array), key); // TODO: reinterpret_cast check
+			memcpy(inout, data_array, BLOCK_SIZE);
 			inout = inout + BLOCK_SIZE;
 		}
 		if (len % BLOCK_SIZE != 0)
 		{
-			int mod = len % BLOCK_SIZE;
-			int offset = (len / BLOCK_SIZE) * BLOCK_SIZE;
+			const auto mod = len % BLOCK_SIZE;
+			const auto offset = len / BLOCK_SIZE * BLOCK_SIZE;
 			uint32_t data[BLOCK_SIZE];
 			memcpy(data, inout + offset, mod);
 			if (encrypt)
-				xtea3_encipher(32, (uint32_t*)data, key);
+				xtea3_encipher(32, static_cast<uint32_t*>(data), key);
 			else
-				xtea3_decipher(32, (uint32_t*)data, key);
+				xtea3_decipher(32, static_cast<uint32_t*>(data), key);
 			memcpy(inout + offset, data, mod);
 		}
 	}
 
 public:
-	XTEA3()
-	{
-	}
+	xtea3() = default;
+	~xtea3() = default;
 
-	~XTEA3()
+	uint8_t* data_crypt(const uint8_t* data, const uint32_t key[8], const uint32_t size)
 	{
-	}
-
-	uint8_t* data_crypt(const uint8_t* data, const uint32_t key[8], uint32_t size)
-	{
-		uint32_t size_crypt_tmp = size;
-		DEBUG_PRINT("CRYPT: \n");
-		DEBUG_PRINT("SIZE = %d \n", size);
+		auto size_crypt_tmp = size;
 		// align to 16
-		while ((size_crypt_tmp % 16) != 0)
+		while (size_crypt_tmp % 16 != 0)
 		{
 			size_crypt_tmp++;
 		}
 		// Allocate memory for aligned buffer
 		// Plus eight bytes, so that there is the size of the encrypted data and the size of the original data, all this will be stored in the encrypted data
-		data_ptr = NULL;
+		data_ptr = nullptr;
 		data_ptr = (uint8_t*)malloc(size_crypt_tmp + 8);
-		if (data_ptr == NULL)
+		if (data_ptr == nullptr)
 		{
-			DEBUG_PRINT("NO FREE MEM \n");
-			return NULL;
+			return nullptr;
 		}
 		// Put the size of the crypted data and the size of the original in the resulting buffer
 		size_crypt = size_crypt_tmp + 8;
@@ -233,12 +223,12 @@ public:
 		if (size_crypt <= size)
 		{
 			// Allocate memory for decrypted data
-			data_ptr = NULL;
+			data_ptr = nullptr;
 			data_ptr = (uint8_t*)malloc(size_crypt);
-			if (data_ptr == NULL)
+			if (data_ptr == nullptr)
 			{
 				DEBUG_PRINT("NO FREE MEM \n");
-				return NULL;
+				return nullptr;
 			}
 			memcpy(data_ptr, data + 8, size_crypt - 8);
 			// Decrypt data
@@ -247,7 +237,7 @@ public:
 		else
 		{
 			DEBUG_PRINT("size_crypt > size \n");
-			return NULL;
+			return nullptr;
 		}
 		return data_ptr;
 	}
@@ -269,11 +259,11 @@ public:
 };
 
 template <size_t N, int K>
-class HideString : protected XTEA3
+class HideString : protected xtea3
 {
 private:
 	const char _key;
-	uint32_t key_for_xtea3[8];
+	uint32_t key_for_xtea3[8]{};
 	uint8_t* crypted_str;
 	std::array<char, N + 1> _encrypted;
 
@@ -291,7 +281,7 @@ public:
 	// Constructor
 	template <size_t... Is>
 	constexpr __forceinline HideString(const char* str, std::index_sequence<Is...>)
-		: _key(RandomChar<K>::value), _encrypted
+		: _key(random_char<K>::value), _encrypted
 		  {
 			  enc(str[Is])...
 
@@ -302,7 +292,7 @@ public:
 		// gen pass for XTEA3
 		for (int i = 0; i < 8; i++)
 		{
-			key_for_xtea3[i] = Murmur3(&value_for_gen_key, sizeof(value_for_gen_key), i);
+			key_for_xtea3[i] = murmur3(&value_for_gen_key, sizeof value_for_gen_key, i);
 		}
 		// crypt
 		crypted_str = data_crypt((const uint8_t*)_encrypted.data(), key_for_xtea3, N);
@@ -315,11 +305,11 @@ public:
 		// gen pass for XTEA3
 		for (int i = 0; i < 8; i++)
 		{
-			key_for_xtea3[i] = Murmur3(&value_for_gen_key, sizeof(value_for_gen_key), i);
+			key_for_xtea3[i] = murmur3(&value_for_gen_key, sizeof value_for_gen_key, i);
 		}
 		// decrypt
 		uint8_t* decrypted_str = data_decrypt(crypted_str, key_for_xtea3, this->get_crypt_size());
-		if (decrypted_str == NULL) return NULL;
+		if (decrypted_str == nullptr) return nullptr;
 		for (size_t i = 0; i < N; ++i)
 		{
 			decrypted_str[i] = dec(decrypted_str[i]);
